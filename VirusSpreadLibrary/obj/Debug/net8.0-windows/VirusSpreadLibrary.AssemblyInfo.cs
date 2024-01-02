f7f023ff3baac1a059533b40d62ebfbebf5f7b52
gitfrid <148685307+gitfrid@users.noreply.github.com>
2024-01-02 02:53:55 +0100

    Implemented enum instead of constants, adapted color list

--- VirusSpreadLibrary/obj/Debug/net8.0-windows/VirusSpreadLibrary.AssemblyInfo.cs
+++ VirusSpreadLibrary/obj/Debug/net8.0-windows/VirusSpreadLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirusSpreadLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05142758d55e78c3c9cee11b6f5ed7e982ece901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirusSpreadLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirusSpreadLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
